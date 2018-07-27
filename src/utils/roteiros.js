142610749ba8d0e28e87ac46a71b567e35eb3a25
kauly <kauly@hotmail.com>
2018-07-26 23:36:57 -0300

    roteiros update

--- src/utils/roteiros.js
+++ src/utils/roteiros.js
@@ -18,79 +18,80 @@ import imgDezesseis from '../img/milao.jpeg';
 const roteiros = [
     {
         title: "Vaticano",
-        text: "Pelas ruas de Roma vamos andar e tal.",
+        text: "Bilhete de entrada do museu do Vaticano. Conhecer a história do mundo.",
         price: "3.00",
         img: imgUm
     },
     {
         title: "Pisa",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: "125.00",
+        text: "Depois de algumas horas de museu, nada como uma pizza italiana e um vinho da casa.",
+        price: "185.00",
         img: imgDois
     },
     {
         title: "Roma",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: "125.00",
+        text: "Dinheiro no bolso para explorar os lugares interessantíssimos e que guardam tesouros inestimáveis de Roma.",
+        price: "95.00",
         img: imgTres
     },
     {
         title: "Roma",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: "125.00",
+        text: "Dizem que a Fontana  di Trevi de madrugada é iluminada e linda. Ideal para uns bons vinhos até ao amanhacer...",
+        price: "215.00",
         img: imgQuatro
     },
     {
         title: "Roma",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: "125.00",
+        text: "Conhecer o Panteão e tudo a sua volta. O resto do dia vamos experimenter o melhor da gastronomia romana nos restaurantes que ficam ao ar livre.",
+        price: "255.00",
         img: imgCinco
     },
     {
         title: "Siena",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: "125.00",
+        text: "Bilhete de entrada Torre del Mangia + Palazzo Pubblico + Santa Maria della Scala. Sim! Nós queremos subir os 505 degraus da Torre del Mangia!",
+        price: "205.00",
         img: imgSeis
     },
     {
         title: "Torre Pisa",
-        text: "Pelas ruas de Roma vamos andar e tal.",
+        text: "Bilhete de entrada da Torre. São 296 degraus!!",
         price: "125.00",
         img: imgSete
     },
     {
         title: "Veneza",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: "125.00",
+        text: "Um lugar, um amor, um final de tarde e um vinho.",
+        price: "145.00",
         img: imgOito
     },
     {
         title: "Veneza",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: "125.00",
+        text: "Parar nos diversos bares de Dorsoduro.Um bairro muito famoso e movimentado da cidade de Veneza.",
+        price: "295.00",
         img: imgNove
     },
     {
         title: "Veneza",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: "125.00",
+        text: "O passeio de Gôndola por Veneza o mais romântico e tradicional.",
+        price: "545.00",
         img: imgDez
     },
     {
         title: "Verona",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: 125,
+        text: "Bilhete de entrada Arena de Verona. Igrejas, templos e palácios fazem parte dessa grande descoberta.",
+        price: "165",
         img: imgOnze
     },
     {
         title: "Verona",
-        text: "Pelas ruas de Roma vamos andar e tal.",
-        price: 125,
+        text: "Em Verona foi escrita a história de amor de Romeu e Julieta, e porque não um jantar sofisticado e um champagne?",
+        price: "325",
         img: imgDoze
-    },    {
+    },    
+    {
         title: "Verona",
         text: "Pelas ruas de Roma vamos andar e tal.",
-        price: 125,
+        price: '125',
         img: imgTreze
     },
     {
